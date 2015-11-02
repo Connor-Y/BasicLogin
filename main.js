60b5eba9acd4af4aa17e7983019041cfc10fdb8b
Connor-Y <connor.y@gmail.com>
2015-11-02 15:48:59 -0500

    Added addition attributes to user Schema, created listUsers()

--- main.js
+++ main.js
@@ -4,61 +4,80 @@ var express = require('express');
 var app = express();
 var mongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
-
+var PORT = 3000;
+var uniqueTestDB = Date();
 
 app.use(express.static('html'));
 app.use(express.static('css'));
 //app.use(express.static('json'));
 
-mongoose.connect('mongodb://localhost/userDb');
+mongoose.connect('mongodb://localhost', PORT);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, "Connection error:"));
 db.once('open', function (callback) {
-	console.log("DB open");
-	var userSchema = mongoose.Schema({
-		username: String,
-		password: String,
-		type: String
-	});
-	console.log("Schema built");
+console.log.bind("Database Loaded"); });
 	
-	userSchema.methods.getData = function () {
-		var data = "Username: " + this.username + " | Usertype: " + this.type;
-		console.log(data);
-	}
-	
-	userSchema.methods.checkLogin = function (name, pass) {
-		if (this.username === name && this.password === pass)
-			console.log("Valid User");
-		else
-			console.log("Invalid Login");
-	}
-	
-	console.log("Methods Added");
-	
-	var User = mongoose.model('User', userSchema);
-	console.log("Model Created");
-	
-	// Test Code - Db insertion
-	var one = new User({username:"Alice", password:"Lord", type:"super"});
-	var two = new User({username:"Bob", password:"second", type:"admin"});
-	var three = new User({username:"Eve", password:"Stalker", type:"user"});
-	console.log("Users added");
+var userSchema = mongoose.Schema({
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+	type: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	image: String,
+	desc: String
+});
+console.log("Schema built");
 
-	//one.save();
-	
-	console.log("---");
+userSchema.methods.getData = function () {
+	var data = "Username: " + this.username + " | Usertype: " + this.type;
+	console.log(data);
+}
+
+userSchema.methods.checkLogin = function (name, pass) {
+	if (this.username === name && this.password === pass)
+		console.log("Valid User");
+	else
+		console.log("Invalid Login");
+}
+
+console.log("Methods Added");
+
+var User = mongoose.model('User', userSchema, uniqueTestDB);
+console.log("Model Created");
+
+// Test Code - Db insertion
+var one = new User({username:"Alice", password:"Lord", type:"super", email:"a"});
+var two = new User({username:"Bob", password:"second", type:"admin", email:"b"});
+var three = new User({username:"Eve", password:"Stalker", type:"user", email:"c"});
+
+one.save();
+two.save();
+three.save();
+
+console.log("Users added");
+console.log("---");
+// Test Code End
+
+function listUsers () {
 	User.find(function (err, users) {
+		console.log("User List:");
 		if (err) 
 			return console.error(err);
 		else
 			console.log(users);
+		console.log("===");
 	});
-	// Test Code End
-	
-	
-	
-});
+}
 
 
-app.listen(3000);
+function addUser (name, pass, email) {
+	console.log("addUser");
+	var newUser = new User({username: name, password: pass, type:"user", email:"d"});
+	newUser.save();
+
+		
+	
+}
+
+addUser("a", "b", "c");
+listUsers();
+app.listen(PORT);
