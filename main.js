fe64198b90378a0b78a5ddafbeaf83cefba3db30
Connor-Y <connor.y@gmail.com>
2015-11-02 17:57:15 -0500

    Cleaned up login page design, created empty html templates for remainder of pages

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ var app = express();
 var mongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var PORT = 3000;
-var uniqueTestDB = Date();
+
+var testNum = "test5";
+var uniqueTestDB = testNum;
 
 app.use(express.static('html'));
 app.use(express.static('css'));
-//app.use(express.static('json'));
 
-mongoose.connect('mongodb://localhost', PORT);
+mongoose.connect('mongodb://localhost/DB', PORT);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, "Connection error:"));
 db.once('open', function (callback) {
@@ -45,17 +46,18 @@ var User = mongoose.model('User', userSchema, uniqueTestDB);
 console.log("Model Created");
 
 // Test Code - Db insertion
-var one = new User({username:"Alice", password:"Lord", type:"super", email:"a"});
-var two = new User({username:"Bob", password:"second", type:"admin", email:"b"});
-var three = new User({username:"Eve", password:"Stalker", type:"user", email:"c"});
+function initDB () {
+	var one = new User({username:"Alice", password:"Lord", type:"super", email:"a"});
+	var two = new User({username:"Bob", password:"second", type:"admin", email:"b"});
+	var three = new User({username:"Eve", password:"Stalker", type:"user", email:"c"});
 
-one.save();
-two.save();
-three.save();
+	one.save();
+	two.save();
+	three.save();
 
-console.log("Users added");
-console.log("---");
-// Test Code End
+	console.log("Users added");
+	console.log("---");	
+} // Test Code End
 
 function listUsers () {
 	User.find(function (err, users) {
@@ -69,15 +71,92 @@ function listUsers () {
 }
 
 
-function addUser (name, pass, email) {
+function addUser (name, pass, mail) {
 	console.log("addUser");
-	var newUser = new User({username: name, password: pass, type:"user", email:"d"});
-	newUser.save();
-
-		
-	
+	User.findOne({ email: mail }, function (err, user) {
+		if (err)
+			return err;
+		if (user == undefined) {
+			console.log("New User");
+			var newUser = new User({username: name, password: pass, type:"user", email:mail});
+			newUser.save();
+		}
+		else
+			console.log("User already exists");
+	});
 }
 
-addUser("a", "b", "c");
-listUsers();
+function userUpdate (currentUser, target, field, newInfo) {
+	console.log("User Update");
+	if (currentUser == undefined || currentUser != target || field == 'email' || field == 'type')
+		return false;
+	var updatedField = {};
+	updatedField[field] = newInfo;
+	var query = { email: target };
+	User.findOneAndUpdate(query, { $set: updatedField },
+	function (err, user) {
+		if (err) {
+			console.bind.error(err);
+			return false;
+		}
+		user.save();
+	});		
+}
+
+// currentUser currently is just a String, howto get user object
+// TODO: ^
+function adminUpdate (currentUser, target, field, newInfo) {
+	console.log("Admin Update");
+	if (currentUser == undefined || currentUser.type != 'admin' || field == 'type')
+		return false;
+	console.log("query");
+	var query = { email: target };
+	if (field == 'email') {
+		User.findOneAndUpdate(query, { $set: {email: newInfo} },
+		function (err, user) {
+			if (err) {
+				console.bind.error(err);
+				return false;
+			}
+			user.save();
+		});
+	}
+	else
+		userUpdate(target, target, field, newInfo);
+}
+
+function superUpdate(currentUser, target, field, newInfo) {
+	if (currentUser == undefined || currentUser.type != 'super')
+		return false;
+	var query = { email: target };
+	if (field == 'type') {
+		User.findOneAndUpdate(query, { $set: {type: newInfo} },
+		function (err, user) {
+			if (err) {
+				console.bind.error(err);
+				return false;
+			}
+			user.save();
+		});
+	}
+	else
+		adminUpdate(currentUser, target, field, newInfo);
+}
+// Doesn't work
+function findUser (mail) {
+	var temp = User.find({ email: mail }, 
+	function (err, user) {
+		return user;
+	});
+}
+
+//initDB();
+//addUser("Mark", "The Dark", "d");
+
+
+//userUpdate('d', 'd', 'image', "Roasted");
+//adminUpdate('b', 'd', 'image', "Trolled");
+
+setTimeout(listUsers, 2000);
+
 app.listen(PORT);
